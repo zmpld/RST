0e496364e7e1b2a29dbcb5c44b2cebac0cf47de9
zmpld <alezandromapalad@gmail.com>
2026-07-08 09:32:16 +0800

    update gitignore

--- obj/Debug/net8.0/RelationshipVisualizer.AssemblyInfo.cs
+++ obj/Debug/net8.0/RelationshipVisualizer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RelationshipVisualizer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e388747cd4a7d31a7f6946c71d82eb850f2a8b8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea10baec02775f7913e455df57483ffabd04e22f")]
 [assembly: System.Reflection.AssemblyProductAttribute("RelationshipVisualizer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RelationshipVisualizer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
